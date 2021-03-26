71822449bea746cacdb14b42d3f2016c642641e7
tedsmitt <edwardsmith92@outlook.com>
2021-03-26 12:17:22 +0000

    Brew formula update for ecsgo version 0.1.1

--- ecsgo.rb
+++ ecsgo.rb
@@ -5,25 +5,25 @@
 class Ecsgo < Formula
   desc "Interactive CLI tool which acts as a wrapper around the ECS ExecuteCommand API."
   homepage ""
-  version "0.1.0"
+  version "0.1.1"
   license "Apache2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.0/ecsgo_Darwin_x86_64.tar.gz"
-    sha256 "ea87a6975664d4ac43f87fc6b35d17266d3e3bf5e72c0393258182ea26cd5357"
+    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.1/ecsgo_Darwin_x86_64.tar.gz"
+    sha256 "5bf8618e8b5df4a4041ac9ada47cef05839be3f990cf1bbc9dd80393b53d0774"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.0/ecsgo_Linux_x86_64.tar.gz"
-    sha256 "f48215245e83eeabda03576a57e121504e4f8a1519b932a23bc862f71ee2217c"
+    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.1/ecsgo_Linux_x86_64.tar.gz"
+    sha256 "0b518d9e5804805240a50d90f1cc56cd3611fb40f7cae7d6b254507bfe1f2027"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.0/ecsgo_Linux_armv6.tar.gz"
-    sha256 "81fe22dfe2b4958a36b4e620192cd106024f70da27c4514133da47e66df4635b"
+    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.1/ecsgo_Linux_armv6.tar.gz"
+    sha256 "5b9fb482041879729df83167df81cee1616280b667bccfe67368989ff2a6785e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.0/ecsgo_Linux_arm64.tar.gz"
-    sha256 "f271d56b7e9e2bdc937e79544d00bbe15ee1dc6051cee60b8a57962157fe705e"
+    url "https://github.com/tedsmitt/ecsgo/releases/download/0.1.1/ecsgo_Linux_arm64.tar.gz"
+    sha256 "6aa6ba76c782e7615783074e8f3bc6447ad4048275a8001d3c41eb87c275ded3"
   end
 
   depends_on "go"
