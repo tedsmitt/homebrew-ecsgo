85c8b90b304bc0f5e01d4c739be1f232beb0b2cb
Ed Smith <edwardsmith92@outlook.com>
2022-03-30 11:22:20 +0100

    Update ecsgo.rb

--- ecsgo.rb
+++ ecsgo.rb
@@ -6,33 +6,42 @@ class Ecsgo < Formula
   desc "Interactive CLI tool which acts as a wrapper around the ECS ExecuteCommand API."
   homepage ""
   version "0.2.0"
-  license "Apache2"
-  bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Darwin_x86_64.tar.gz"
-    sha256 "a7dd798605b8ecc6534d746f9c879136a46a37a838fe0cf4d1d1f3e8ac82d56c"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Darwin_arm64.tar.gz"
-    sha256 "213369f25b19cb0d90fdd0a697815c352d5bd2c277a3259a1bdc98d960375ea5"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Linux_x86_64.tar.gz"
-    sha256 "9e60f391a5e4fd7c05a08d74c2426b665c8815a26029bd5a16d3d4e11509b5c9"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Linux_arm64.tar.gz"
-    sha256 "4660b1d6fbe7232d7a3fba4fdefbda0f6496ae8fb4ab5b1ebafc98f9609de3a8"
-  end
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Darwin_arm64.tar.gz"
+      sha256 "213369f25b19cb0d90fdd0a697815c352d5bd2c277a3259a1bdc98d960375ea5"
 
-  depends_on "go"
+      def install
+        bin.install "gossm"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Darwin_x86_64.tar.gz"
+      sha256 "a7dd798605b8ecc6534d746f9c879136a46a37a838fe0cf4d1d1f3e8ac82d56c"
 
-  def install
-    bin.install "ecsgo"
+      def install
+        bin.install "gossm"
+      end
+    end
   end
 
-  test do
-    system "#{bin}/goreleaser -v"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Linux_arm64.tar.gz"
+      sha256 "4660b1d6fbe7232d7a3fba4fdefbda0f6496ae8fb4ab5b1ebafc98f9609de3a8"
+
+      def install
+        bin.install "gossm"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/tedsmitt/ecsgo/releases/download/0.2.0/ecsgo_Linux_x86_64.tar.gz"
+      sha256 "9e60f391a5e4fd7c05a08d74c2426b665c8815a26029bd5a16d3d4e11509b5c9"
+
+      def install
+        bin.install "gossm"
+      end
+    end
   end
 end
